078a3b867213a79d9301ac3a9107e3793389ea9d
OIC-Shinchaemin <shin@oic.jp>
2021-11-22 16:18:22 +0900

    24-2まで
    関数化でもやらないと

--- Chapter24-1 アドベンチャーゲーム(画面構成)/Project/Game.cpp
+++ Chapter24-1 アドベンチャーゲーム(画面構成)/Project/Game.cpp
@@ -2,6 +2,8 @@
 #include	"Game.h"
 #include	"Save.h"
 
+// TODO:関数化
+
 //現在のシーン(外部参照、実体はGameApp.cpp)
 extern int						gCurrentScene;
 //変更するシーン(外部参照、実体はGameApp.cpp)
@@ -17,7 +19,16 @@ extern CSave					gSaveScene;
  */
 CGame::CGame() :
 m_Alpha(0) ,
-m_bEnd(false) {
+m_bEnd(false),
+m_TextWindow(),
+m_Script(),
+m_SpriteList(),
+m_TextCommand(),
+m_StrWait(0),
+m_CommandNo(0),
+m_pNowCommand(NULL),
+m_bWait(false) {
+	memset(m_LineBuffer, 0, TEXTBUFFERSIZE);
 }
 
 /**
@@ -32,6 +43,34 @@ CGame::~CGame(){
  * 利用するテクスチャを読み込む。
  */
 bool CGame::Load(void){
+	//スクリプトを読み込む
+	if (!LoadScript("test.txt"))
+	{
+		return false;
+	}
+	//テキスト表示用のウインドウを読み込む
+	if (!m_TextWindow.Load("TextWindow.png"))
+	{
+		return false;
+	}
+	return true;
+}
+
+bool CGame::LoadScript(const char* name) 
+{
+	//現在の情報を解放
+	m_Script.Release();
+	m_SpriteList.Release();
+
+	m_CommandNo = 0;
+	m_bWait = false;
+	//スクリプトの読み込み
+	if (!m_Script.Load(name))
+	{
+		return false;
+	}
+	//ウェイト処理までを実行
+	StepCommand();
 	return true;
 }
 
@@ -75,6 +114,12 @@ void CGame::UpdateAlpha(void){
 			m_Alpha += ALPHA_SPEED;
 		}
 	}
+	//スプライトのアルファ値をシーンのアルファ値と同じにする
+	//スプライトのアルファメンバは 0.0f ～ 1.0f になるため 255 で割った値を設定する。
+	for (int i = 0; i < m_SpriteList.GetArrayCount(); i++)
+	{
+		m_SpriteList[i]->m_Color.a = m_Alpha / 255.0f;
+	}
 }
 
 /**
@@ -89,6 +134,28 @@ void CGame::Update(void){
 	if(m_bEnd || gCurrentScene != gNextScene)
 	{
 		return;
+	}
+
+	//スクリプトによる更新
+	if (m_bWait)
+	{
+		//コマンドのタイプによって分岐
+		switch (m_pNowCommand->Type)
+		{
+		case CMD_TEXT:
+			TextCommand();
+			break;
+		}
+	}
+	//クリックで次のコマンドから実行を再開
+	else if (g_pInput->IsMouseKeyPush(MOFMOUSE_LBUTTON))
+	{
+		StepCommand();
+	}
+	//スプライトのリストを更新する
+	for (int i = 0; i < m_SpriteList.GetArrayCount(); i++)
+	{
+		m_SpriteList[i]->Update();
 	}
 
 	//F2キーでタイトル画面へ
@@ -106,24 +173,155 @@ void CGame::Update(void){
 	}
 }
 
+void CGame::StepCommand(void) {
+	//すべてのコマンドを実行するか待機処理に入るまでループ実行
+	while (m_CommandNo < m_Script.GetCommandCount() && !m_bWait)
+	{
+		//実行コマンド取り出し
+		m_pNowCommand = m_Script.GetCommand(m_CommandNo);
+		//コマンドのタイプによって分岐
+		switch (m_pNowCommand->Type)
+		{
+		case CMD_TEXT:
+			m_TextCommand = *((TEXTCOMMAND*)m_pNowCommand);
+			memset(m_LineBuffer, 0, TEXTBUFFERSIZE);
+			m_StrWait = 0;
+			m_bWait = true;
+			break;
+		case CMD_SPRITE:
+			SpriteCommand((SPRITECOMMAND*)m_pNowCommand);
+			break;
+		case CMD_SETPOS:
+			SetPosCommand((SETPOSCOMMAND*)m_pNowCommand);
+			break;
+		case CMD_SETSHOW: //スプライトの表示設定コマンド
+			SetShowCommand((SETSHOWCOMMAND*)m_pNowCommand);
+			break;
+		default: //定義されていないコマンド
+			break;
+		}
+		//次のコマンドへ
+		m_CommandNo++;
+	}
+}
+/**
+* テキストコマンドの処理
+*/
+void CGame::TextCommand(void) {
+	//表示中のテキストと、表示するテキストの長さを取得
+	int nl = strlen(m_LineBuffer);
+	int tl = strlen(m_TextCommand.Text);
+	//全部表示された
+	if (nl >= tl) { return; }
+	//クリックで全文を一括表示
+	if (g_pInput->IsMouseKeyPush(MOFMOUSE_LBUTTON))
+	{
+		m_bWait = false;
+		strcpy(m_LineBuffer, m_TextCommand.Text);
+		return;
+	}
+	//一定時間ごとに一文字ずつ入れていく
+	m_StrWait++;
+	if (m_StrWait < 10) { return; }
+	m_StrWait = 0;
+	//全角文字の判定を行う。全角文字の場合２バイト分で一文字になるので 1 文字分処理追加
+	if (IsDBCSLeadByte(m_TextCommand.Text[nl]))
+	{
+		m_LineBuffer[nl] = m_TextCommand.Text[nl];
+		nl++;
+	}
+	m_LineBuffer[nl] = m_TextCommand.Text[nl];
+	nl++;
+	m_LineBuffer[nl] = '\0';
+	//全文の表示が終了
+	if (nl >= tl)
+	{
+		m_bWait = false;
+	}
+}
+/**
+* スプライトコマンドの処理
+*
+* 引数
+* [in] pSpriteCommand コマンド
+*/
+void CGame::SpriteCommand(SPRITECOMMAND* pSpriteCommand) {
+	LPSprite2D* pSprite = m_SpriteList.Find(pSpriteCommand->Name);
+	//スプライトがない場合は新規作成してリストに登録
+	if (!pSprite)
+	{
+		CSprite2D* pAdd = new CSprite2D(pSpriteCommand->FName);
+		pAdd->SetName(pSpriteCommand->Name);
+		m_SpriteList.Add(&pAdd);
+	}
+	//スプライトがある場合古い情報を破棄して新しいテクスチャファイルに差し替え
+	else
+	{
+		(*pSprite)->Release();
+		(*pSprite)->CreateSprite(pSpriteCommand->FName);
+		(*pSprite)->SetName(pSpriteCommand->Name);
+	}
+}
+/**
+* スプライト座標設定コマンドの処理
+*
+* 引数
+* [in] pSpriteCommand コマンド
+*/
+void CGame::SetPosCommand(SETPOSCOMMAND* pSpriteCommand) {
+	LPSprite2D* pSprite = m_SpriteList.Find(pSpriteCommand->Name);
+	if (!pSprite) { return; }
+	(*pSprite)->m_Position.x = pSpriteCommand->px;
+	(*pSprite)->m_Position.y = pSpriteCommand->py;
+}
+/**
+* スプライト表示設定コマンドの処理
+*
+* 引数
+* [in] pSpriteCommand コマンド
+*/
+void CGame::SetShowCommand(SETSHOWCOMMAND* pSpriteCommand) {
+	LPSprite2D* pSprite = m_SpriteList.Find(pSpriteCommand->Name);
+	if (!pSprite) { return; }
+	(*pSprite)->m_bShow = pSpriteCommand->bShow;
+}
+
 /**
  * 描画
  *
  */
 void CGame::Render(void){
 	CGraphicsUtilities::RenderString(10,10,MOF_ARGB(m_Alpha,255,0,0),"ゲーム画面");
+
+	//スプライトのリストを描画する
+	for (int i = 0; i < m_SpriteList.GetArrayCount(); i++)
+	{
+		m_SpriteList[i]->Render();
+	}
+	//表示テキストの下に枠を表示する
+	m_TextWindow.Render(16, 568);
+	//表示テキストを描画する
+	CGraphicsUtilities::RenderString(m_TextCommand.px, m_TextCommand.py,
+		MOF_ARGB(m_Alpha, 255, 255, 255), m_LineBuffer);
 }
 
 /**
  * デバッグ描画
  *
  */
-void CGame::RenderDebug(void){
+void CGame::RenderDebug(void)
+{
+	CGraphicsUtilities::RenderString(10, 10, MOF_ARGB(m_Alpha, 255, 0, 0),
+		"コマンド実行位置 [%d]", m_CommandNo);
 }
 
 /**
  * 解放
  *
  */
-void CGame::Release(void){
+void CGame::Release(void)
+{
+	m_TextWindow.Release();
+	m_Script.Release();
+	m_SpriteList.Release();
 }
--- Chapter24-1 アドベンチャーゲーム(画面構成)/Project/Game.h
+++ Chapter24-1 アドベンチャーゲーム(画面構成)/Project/Game.h
@@ -1,9 +1,21 @@
 #pragma once
 
+#include "Script.h"
+
 class CGame {
 private:
-	BYTE					m_Alpha;
-	bool					m_bEnd;
+	BYTE						m_Alpha;
+	bool						m_bEnd;
+	CTexture					m_TextWindow;
+	CScript						m_Script;
+	CDynamicArray<CSprite2D*>	m_SpriteList;
+	TEXTCOMMAND					m_TextCommand;
+	char						m_LineBuffer[256];
+	int							m_StrWait;
+	int							m_CommandNo;
+	COMMAND*					m_pNowCommand;
+	bool						m_bWait;
+
 public:
 	CGame();
 	~CGame();
@@ -14,4 +26,12 @@ public:
 	void Render(void);
 	void RenderDebug(void);
 	void Release(void);
+
+	bool LoadScript(const char* name);
+	void StepCommand();
+	void TextCommand();
+
+	void SpriteCommand(SPRITECOMMAND* pSpritCommand);
+	void SetPosCommand(SETPOSCOMMAND* pSetPosCommand);
+	void SetShowCommand(SETSHOWCOMMAND* pSetShowCommand);
 };
